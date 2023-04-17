fbb1637c0ba5d4e4cc09ef4be72f772fd94eb78b
oyarso <sam.copae@gmail.com>
2023-04-17 03:59:20 -0400

    Corregido navbar

--- assets/js/instruction.js
+++ assets/js/instruction.js
@@ -29,29 +29,24 @@ function tcg(digimon) {
     contenido.innerHTML += `
 
     <div class="card item col-xs-8 col-sm-6 col-md-4 col-lg-3">
-<div class="container">
-<div class="card-body">
-<h4 class="card-title">${digimon.name}</h4>
-<p class="card-text">${digimon.level}</p>
-</div>
+      <div class="container">
+        <div class="card-body">
+        <h4 class="card-title">${digimon.name}</h4>
+        <p class="card-text">${digimon.level}</p>
+      </div>
+        <img src="${digimon.img}" alt="Digi" class="image">
+          <div class="overlay">
+            <div class="text">
+              <div class="card-body">
+                <h4 class="card-title">${digimon.name}</h4>
+                <p class="card-text">${digimon.level}</p><br>
+                <input type="button" class="btn btn-outline-warning" value="Click aqui" onClick="Alerta()">
+              </div>
+            </div>
+          </div>
+      </div>
 
-  <img src="${digimon.img}" alt="Digi" class="image">
-  <div class="overlay">
-    <div class="text">
 
-    <div class="card-body">
-    <h4 class="card-title">${digimon.name}</h4>
-    <p class="card-text">${digimon.level}</p>
-    <input type="button" class="btn btn-outline-warning" value="Click aqui" onClick="Alerta()">
-
-
-</div>
-
-    </div>
-  </div>
-</div>
-</div>
-   
     
     `
 }
